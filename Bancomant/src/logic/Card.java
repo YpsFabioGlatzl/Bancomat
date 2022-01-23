5e72ec2ed0b27940c1c15dfc4b49e7156e3c1c0e
YpsFabioGlatzl <97897203+YpsFabioGlatzl@users.noreply.github.com>
2022-01-23 23:03:36 +0100

    Update Card.java

--- Bancomant/src/logic/Card.java
+++ Bancomant/src/logic/Card.java
@@ -5,10 +5,40 @@
  */
 package logic;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author fglat
  */
 public class Card {
+    private String pathToCards = "../data/";
+    private String nameHolder;
+    private String surnameHolder;
+    private String nameConto;
+    private String IBAN;
+    private String bank;
+    private String cardNumber;
+    private String validTill;
+    private int pin;
+    
+    
+    private boolean checkCard(String cardName) throws FileNotFoundException{
+        pathToCards += cardName;
+        Scanner s = new Scanner(new File(pathToCards));
+        ArrayList<String> data = new ArrayList<String>();
+        while (s.hasNext()){
+            data.add(s.next());
+        }
+        s.close();
+        
+        
+        
+        
+        return false;
+    }
     
 }
